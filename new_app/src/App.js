3618200355bb66851dd51e694b9aab2095d76eec
210-01\210-01 <210>
2022-03-31 21:36:41 +0500

    no message

--- new_app/src/App.js
+++ new_app/src/App.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
+import io from 'socket.io-client'
 import Home from './panels/Home';
 import Persik from './panels/Persik';
+const SERVER_URL = 'http://localhost:5000'
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
 	const [fetchedUser, setUser] = useState(null);
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	
+	const [messages, setMessages ] = useState ([])
 
-	useEffect(() => {
+	const SERVER_URL = 'https://vk-mini-server-learn.herokuapp.com'
+	const sendMessage =({messageText,senderName}) =>{
+		socket.current.emit('message:add',{
+			userId:fetchedUser.id,
+			messageText,
+			senderName
+		})
+	}
+
+	const socket=useRef(null)
+
+	useEffect(async () => {
 		bridge.subscribe(({ detail: { type, data }}) => {
 			if (type === 'VKWebAppUpdateConfig') {
 				const schemeAttribute = document.createAttribute('scheme');
@@ -19,23 +34,44 @@ const App = () => {
 				document.body.attributes.setNamedItem(schemeAttribute);
 			}
 		});
+		let user = null
 		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
+			user = await bridge.send('VKWebAppGetUserInfo');
 			setUser(user);
 			setPopout(null);
+			socket.current = io(SERVER_URL)
+	
+	     socket.current.emit ('message:get')
 		}
-		fetchData();
+	await	fetchData();
+
+		console.log('useEffect')
+	
+	    socket.current.on ('messages',(messages) => {
+		const newMessages = messages.map((msg)=>
+		msg.userId === user.id ? {
+			...msg, currentUser:true
+		}:msg)
+
+		setMessages(newMessages)
+	})
+
+		return () => {
+			socket.current.disconnect()
+		}
+
 	}, []);
 
 	const go = e => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
+	
 	return (
 		<AdaptivityProvider>
 			<AppRoot>
 				<View activePanel={activePanel} popout={popout}>
-					<Home id='home' fetchedUser={fetchedUser} go={go} />
+					<Home id='home' fetchedUser={fetchedUser} go={go} messages={messages} sendMessage={sendMessage} />
 					<Persik id='persik' go={go} />
 				</View>
 			</AppRoot>
@@ -43,4 +79,6 @@ const App = () => {
 	);
 }
 
+
 export default App;
+
--- new_app/src/panels/Home.js
+++ new_app/src/panels/Home.js
@@ -1,19 +1,43 @@
-import React, { useState, Fragment  } from 'react';
+import React, {useState, Fragment} from 'react';
 import PropTypes from 'prop-types';
 
-import { WriteBarIcon, WriteBar, Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, ContentCard, CardGrid, FixedLayout, Separator } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, CardGrid,ContentCard, FixedLayout, Separator,WriteBar, WriteBarIcon } from '@vkontakte/vkui';
 
 
 
-const Home = ({ id, go, fetchedUser }) => {
-	const [text, setText] = useState("");
+const SERVER_URL = 'http://localhost:5000'
+
+const Home = ({ id, go, fetchedUser, messages, sendMessage }) => {
+	const [text, setText] = useState(""); 
+
+
+	
+	const handleChangeText = (e) => {
+		setText (e.target.value)
+	}
+
+	const handleSendMessage = (e) => {
+		e.preventDefault ()
+		const trimmed = text.trim()
+		if (trimmed){
+			sendMessage ({
+				messageText: text, senderName: `${fetchedUser.first_name} ${fetchedUser.last_name}`
+			})
+			setText ('')
+		}
+	}
+
+
+
+
 	return (
 	<Panel id={id}>
-		<PanelHeader>Slogno</PanelHeader>
 
 
-		{/*fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+		<PanelHeader>Люль</PanelHeader>
+		
+		 {/*fetchedUser &&
+		<Group header={<Header mode="secondary">Добро пожаловать!</Header>}>
 			<Cell
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
 				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
@@ -22,64 +46,50 @@ const Home = ({ id, go, fetchedUser }) => {
 			</Cell>
 		</Group>}
 
-		<Group header={<Header mode="secondary">Ochen Slogno</Header>}>
+		<Group header={<Header mode="secondary">Navigation Example</Header>}>
 			<Div>
-				<Button stretched size="1" mode="secondary" onClick={go} data-to="persik">
-					Поставьте зачетик)
+				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
+					Show me the Persik, please
 				</Button>
 			</Div>
-</Group>*/}
-		
+		</Group> 
+*/} 
+
 		<Group>
-          <CardGrid size="l">
-            <ContentCard
-              subtitle="VKUI"
-              header="ContentCard example"
-              caption="VKUI Styleguide > Blocks > ContentCard"
-            />
-            <ContentCard
-              onClick={() => {}}
-              src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-              subtitle="unsplash"
-              header="brown and gray mountains under blue sky during daytime photo"
-              text="Mountain changji"
-              caption="Photo by Siyuan on Unsplash"
-              maxHeight={150}
-            />
-            <ContentCard
-              disabled
-              src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-              subtitle="unsplash"
-              header="persons left hand with pink paint"
-              text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
-              caption="Photo by Alexander Jawfox on Unsplash"
-              maxHeight={500}
-            />
-          </CardGrid>
+		<CardGrid size="l">
+			{messages.map((msg)=>(
+			<ContentCard
+			src={msg.avatar}
+			header={msg.senderName}
+			text={msg.messageText}
+			caption={msg.createdAt}
+			/>
+			))}
+			</CardGrid>
         </Group>
 
-		<FixedLayout vertical="bottom">
+          <FixedLayout vertical="bottom">
         
             <Separator wide />
-
            
-
             <WriteBar
-             
+            
               after={
                 <Fragment>
-					<WriteBarIcon mode="send" />
+				  <WriteBarIcon mode="send" 
+				  onClick = {handleSendMessage} />
+
                 </Fragment>
               }
               value={text}
-              onChange={(e) => setText(e.target.value)}
-             
+              onChange={handleChangeText}
+          
               placeholder="Сообщение"
             />
-        
-        </FixedLayout>
+          
+        </FixedLayout>		
+	</Panel> )
 
-	</Panel>)
 };
 
 Home.propTypes = {
